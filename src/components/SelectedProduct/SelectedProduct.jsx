a236edb8d92075c7558c1082968964d9090edcac
Lucas <lucasnobreoficial@hotmail.com>
2025-01-04 20:08:24 -0300

    melhoria no design de produto selecionado

--- src/components/SelectedProduct/SelectedProduct.jsx
+++ src/components/SelectedProduct/SelectedProduct.jsx
@@ -9,13 +9,16 @@ const SelectedProduct = ({ tittle, author, price, image, path }) => {
       <div className="flex items-center justify-center gap-14 p-10 selected-product-container">
         <Link to={path}><img src={arrowBack} alt="Voltar" className="return-button"/></Link>
         <div>
-          <img src={image} alt="test" className="w-64 p-1" />
+          <img src={image} alt="test" className="product-item-image"/>
         </div>
         <div className="info-container">
           <div className="product-details">
-            <h1 className="product-tittle">{tittle}</h1>
-            <h3 className="product-author">Por {author}</h3>
-            <p className="product-price">R${price}</p>
+            <p className="for-only">Por apenas</p>
+            <div className="product-item-information">
+              <p className="product-item-price">R${price}</p>
+              <h1 className="product-item-tittle">{tittle}</h1>
+              <h3 className="product-item-author">Por <span className="author-stlye">{author}</span></h3>
+            </div>
           </div>
           <button className="buy-button">Comprar agora</button>
         </div>
